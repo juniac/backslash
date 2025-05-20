f44a8708d12d265f57b43aff49e1cb95b2160920
Jeong-Un, Kim <juniac79@gmail.com>
2025-05-20 12:41:07 +0900

    Fix focus on coupang search input when / key pushed

--- src/content/coupang.ts
+++ src/content/coupang.ts
@@ -1,10 +1,13 @@
 export function activateSlashKeyOnCoupang() {
-  console.log('coupang slash ');
-  document.addEventListener('keydown', (e: KeyboardEvent) => {
-    if (e.key === '/') {
-      console.log('move');
-      document.getElementById('headerSearchKeyword')?.focus();
-      e.preventDefault();
+  console.log("coupang slash ")
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (e.key === "/") {
+      // document.getElementById("headerSearchKeyword")?.focus()
+      const searchInput = document.querySelector(
+        "form#wa-search-form input[name='q']"
+      ) as HTMLInputElement
+      searchInput?.focus()
+      e.preventDefault()
     }
-  });
+  })
 }
